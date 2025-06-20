54c663985a31ff968773b8842a8c536f7e752ca9
Hitesh Makvana <hitesh.r.makvana@techgrains.com>
2025-06-20 12:44:03 +0530

    Initial Commit

---
+++ MockServiceSwiftUIApp/MockServiceSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockServiceSwiftUIAppApp.swift
+//  MockServiceSwiftUIApp
+//
+//  Created by admin on 20/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MockServiceSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MockServiceSwiftUIAppTests/MockServiceSwiftUIAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MockServiceSwiftUIAppTests.swift
+//  MockServiceSwiftUIAppTests
+//
+//  Created by admin on 20/06/25.
+//
+
+import Testing
+@testable import MockServiceSwiftUIApp
+
+struct MockServiceSwiftUIAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
